50ab1b5051e9621591a9e3062c0ed2a7d9b5f972
Ewerton <Ewerton@DESKTOP-MN9GPG8>
2025-01-26 16:27:56 -0300

    ajuste de import

--- pages/cadastro/produto/index.tsx
+++ pages/cadastro/produto/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import ProdutoGrid from "@/components/produto/ProdutoGrid";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import {
